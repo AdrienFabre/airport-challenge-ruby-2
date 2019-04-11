70da4bc8b6a640fbd7054ca83dc04cae656abaf8
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-04-11 16:19:04 +0100

    User Story 9 As an air traffic controller So the system is consistent and correctly reports plane status and location I want to ensure a plane that is not flying cannot land and must be in an airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,10 +1,22 @@
 class Plane 
 
+  def initialize
+    @flying = true
+  end
+
   def take_off
-    raise "Plane can not take off: plane already flying"
+    raise "Plane can not take off: plane already flying" if @flying
+    @flying = true
+  end
+
+  def land(airport)
+    raise 'Plane can not land: plane already landed' unless @flying
+    @flying = false
+    @airport = airport
   end
 
   def airport
-    raise 'Plane can not be in an airport: plane already flying'
+    raise 'Plane can not be in an airport: plane already flying' if @flying
+    @airport
   end
 end 
